74d28796ba972da3e444bda045a4c576137eeb6b
javafa <nigne2016@gmail.com>
2016-10-21 16:12:24 +0900

    최초등록

---
+++ app/src/main/java/com/kodonho/android/remote_httpurlconnection/MainActivity.java
@@ -0,0 +1,80 @@
+package com.kodonho.android.remote_httpurlconnection;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getNaver();
+            }
+        });
+    }
+
+    private void getNaver(){
+
+        new AsyncTask<Void, Void, String>(){
+            ProgressDialog progress;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                progress = new ProgressDialog(MainActivity.this);
+                progress.setTitle("다운로드");
+                progress.setMessage("downloading...");
+                progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                progress.setCancelable(false);
+                progress.show();
+            }
+            @Override
+            protected String doInBackground(Void... params) {
+                String result = "";
+                try { result = Remote.getData("http://swopenapi.seoul.go.kr/api/subway/sample/json/realtimeStationArrival/0/5/%EC%84%9C%EC%9A%B8"); }catch(Exception e){ e.printStackTrace(); }
+                return result;
+            }
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                StringBuffer sb = new StringBuffer();
+                try {
+                    // 전체 문자열을 JSON 오브젝트로 변환
+                    JSONObject json = new JSONObject(s);
+                    // 특정 키를 가진 단일값을 꺼낸다
+                    //JSONObject topObject = json.getJSONObject("realtimeStationArrival");
+                    // 특정 키를 가진 배열형태의 값을 꺼낸다
+                    JSONArray rows = json.getJSONArray("realtimeArrivalList");
+                    int rows_count = rows.length();
+                    for (int i = 0; i < rows_count; i++) {
+                        // 배열을 반복문을 돌면서 배열의 index로 값을 꺼낸다
+                        JSONObject result = (JSONObject) rows.get(i);
+                        // 최종적으로 각열의 컬럼의 키이름에 해당하는 값을 꺼낸다
+                        String trainName = result.getString("trainLineNm");
+                        sb.append(trainName + "\n");
+                    }
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+                tv.setText(sb.toString());
+                progress.dismiss();
+            }
+        }.execute();
+    }
+}
